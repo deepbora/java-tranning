458c5735cb7748c9938b4674d586a8473c2d1e5d
Deepjyoti Bora <59271987+deepbora@users.noreply.github.com>
2022-05-23 04:14:00 +0530

    packages

---
+++ src/mycompany/Package_call.java
@@ -0,0 +1,19 @@
+
+//package mycompany;
+
+//import mycompany.Package_1;
+import Newpackege.Package_1_new;
+public class Package_call {
+
+  
+    public static void main(String[] args) {
+        
+        Package_1_new o1=new Package_1_new();
+        o1.display();
+        o1.run();
+        
+    }
+
+   
+    
+}
