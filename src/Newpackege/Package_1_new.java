7812d141dce26ef5edcb1074903aa3ee835086f3
Deepjyoti Bora <59271987+deepbora@users.noreply.github.com>
2022-05-23 04:14:15 +0530

    packages

---
+++ src/Newpackege/Package_1_new.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Newpackege;
+
+public class Package_1_new {
+    
+    
+    public void display(){
+        System.out.println("NewPackage sucssesfully called");
+    }
+    public void run(){
+        
+        System.out.println("NewRun Method is called");
+        
+        
+    }
+}
