bea03fcd1f178236cfd7a98643fb243c2f749223
Deepjyoti Bora <59271987+deepbora@users.noreply.github.com>
2022-05-23 04:13:16 +0530

    packages

---
+++ src/mycompany/Package_1.java
@@ -0,0 +1,16 @@
+
+package mycompany;
+
+public class Package_1 {
+    
+    
+    public void display(){
+        System.out.println("Package sucssesfully called");
+    }
+    public void run(){
+        
+        System.out.println("Run Method is called");
+        
+        
+    }
+}
