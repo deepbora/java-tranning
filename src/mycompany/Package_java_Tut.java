90f2cc3a61db2392da03645304aa5acd54882097
Deepjyoti Bora <59271987+deepbora@users.noreply.github.com>
2022-05-23 04:12:30 +0530

    packages

---
+++ src/mycompany/Package_java_Tut.java
@@ -0,0 +1,57 @@
+
+
+
+
+package mycompany;
+
+/*
+java package--
+ package is used to group related class.
+java package act as "container" for classess ...
+package hepls avoid name conflic ...
+(package is a conainer of to related group of class)
+
+
+types of package--
+
+.Bulit in package--java api(import package_name.class_name)
+.User-define package--custom package
+
+
+
+package name--
+
+.java.lang--->string,math,threads,exception(for all--*)
+
+.java.util--->tables,random,date,Scanner(for all--*)
+
+.java.io--->(for all--*)
+
+.java.awt-->(for all--*)
+
+.java.net-->(for all--*)
+
+.java.applet-->(for all--*)
+
+Mehod--
+1)import java.lang.string;
+2)import java.lang.*;
+3)String s=java.lang.string("deep");
+
+Creating Custom java package using commond--
+
+pachage ---package_name---;
+
+or--
+package --package_name.subpackage_name--;
+
+for exicute commond---
+
+javac -d . file.java
+
+for all--
+javac -d . *.java
+
+
+*/
+
